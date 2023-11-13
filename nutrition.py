a7eea8dee801cda0275da9b7c9c3033cf4910169
scheinbein <150675203+scheinbein@users.noreply.github.com>
2023-11-13 09:46:33 -0300

    nutrition.py

---
+++ nutrition.py
@@ -0,0 +1,42 @@
+fruta = input('Digite o nome de uma fruta: ').casefold()
+
+if fruta == 'apple':
+    print('130 calorias')
+elif fruta == 'avocado':
+    print('50 calorias')
+elif fruta == 'banana':
+    print('110 calorias')
+elif fruta == 'cantaloupe':
+    print('50 calorias')
+elif fruta == 'grapefruit':
+    print('60 calorias')
+elif fruta == 'grapes':
+    print('90 calorias')
+elif fruta == 'honeydew melon':
+    print('50 calorias')
+elif fruta == 'kiwifruit':
+    print('90 calorias')
+elif fruta == 'lemon':
+    print('15 calorias')
+elif fruta == 'lime':
+    print('20 calorias')
+elif fruta == 'nectarine':
+    print('60 calorias')
+elif fruta == 'orange':
+    print('80 calorias')
+elif fruta == 'peach':
+    print('60 calorias')
+elif fruta == 'pear':
+    print('100 calorias')
+elif fruta == 'pineapple':
+    print('50 calorias')
+elif fruta == 'plums':
+    print('70 calorias')
+elif fruta == 'strawberries':
+    print('50 calorias')
+elif fruta == 'sweet cherries':
+    print('100 calorias')
+elif fruta == 'tangerine':
+    print('50 calorias')
+elif fruta == 'watermelon':
+    print('80 calorias')
